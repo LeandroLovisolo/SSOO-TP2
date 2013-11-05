2a3dfe42538b1c228dd57f5aa002ae3622c92014
Lautaro Petaccio <lausuper@gmail.com>
2013-11-05 20:13:06 -0300

    Inicializacion de mutex

--- backend-multi/RWLock.cpp
+++ backend-multi/RWLock.cpp
@@ -1,21 +1,25 @@
 #include "RWLock.h"
 
-RWLock :: RWLock() {
+RWLock::RWLock() {
+	//Inicializo el mutex
+	pthread_mutex_init(&mutex, NULL);
+}
+
+void RWLock::rlock() {
 
 }
 
-void RWLock :: rlock() {
+void RWLock::wlock() {
 
 }
 
-void RWLock :: wlock() {
+void RWLock::runlock() {
 
 }
 
-void RWLock :: runlock() {
+void RWLock::wunlock() {
 
 }
 
-void RWLock :: wunlock() {
-
-}
+RWLock::~RWLock() {
+pthread_mutex_destroy(&mutex)
--- backend-multi/RWLock.h
+++ backend-multi/RWLock.h
@@ -1,16 +1,19 @@
 #ifndef RWLock_h
 #define RWLock_h
 #include <iostream>
+#include <pthread.h>
 
 class RWLock {
     public:
         RWLock();
+        ~RWLock();
         void rlock();
         void wlock();
         void runlock();
         void wunlock();
 
     private:
+    	pthread_mutex_t mutex;
 
 };
 
