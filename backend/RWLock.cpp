aa022280244f69294de19612b352a67c5b78449d
Leandro Lovisolo <leandro@leandro.me>
2013-11-14 02:29:14 -0300

    Macro para deshabilitar protección contra inanición de escritura

--- backend/RWLock.cpp
+++ backend/RWLock.cpp
@@ -6,6 +6,10 @@
 // Lock readers-writers sin inanición, tomado de The Little Book of Semaphores,
 // versión 2.1.5, por Allen B. Downey; página 79.
 
+// Comentar la siguiente línea para obtener la implementación con inanición,
+// para verificar que lo logra pasar los tests de inanición en tests.cpp.
+#define SIN_INANICION
+
 RWLock::RWLock() {
 	pthread_mutex_init(&turnstile, NULL);
 	pthread_mutex_init(&room_empty, NULL);
@@ -20,8 +24,10 @@ RWLock::~RWLock() {
 }
 
 void RWLock::rlock() {
+	#ifdef SIN_INANICION
 	wait(turnstile);
 	signal(turnstile);
+	#endif
 	lock_lightswitch();
 }
 
@@ -30,12 +36,16 @@ void RWLock::runlock() {
 }
 
 void RWLock::wlock() {
+	#ifdef SIN_INANICION
 	wait(turnstile);
+	#endif
 	wait(room_empty);
 }
 
 void RWLock::wunlock() {
+	#ifdef SIN_INANICION
 	signal(turnstile);
+	#endif
 	signal(room_empty);
 }
 
